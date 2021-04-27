c40ea58b6dbe1d448e0c47c82c9166a54100b272
Keenan-Schissel <77352533+Keenan-Schissel@users.noreply.github.com>
2021-04-27 10:39:35 -0500

    Initial commit

---
+++ lab-6-anova.R
@@ -0,0 +1,21 @@
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse)
+
+
+# read data ---------------------------------------------------------------
+
+crabs <- read_csv("chap15q27FiddlerCrabFans.csv")
+crabs
+
+# QUESTION D --------------------------------------------------------------
+
+# graph the distribution of body temperatures for each crab type
+
+
+
+# QUESTION E --------------------------------------------------------------
+
+# ANOVA
+
---
+++ lab-6-ttest.R
@@ -0,0 +1,30 @@
+
+# load packages -----------------------------------------------------------
+
+library(tidyverse)
+
+
+# read data ---------------------------------------------------------------
+
+fish <- read_csv("chap12q19ElectricFish.csv")
+
+
+# put data in tidy format ------------------------------------------------
+
+fish_long <- 
+  pivot_longer(fish, speciesUpstream:speciesDownstream,
+               names_to = "location",
+               values_to = "species") %>% 
+  mutate(location = str_remove(location, c("species"))) %>% 
+  print()
+
+
+# Question A t-test -------------------------------------------------------
+
+
+# Question B difference in means -----------------------------------------
+
+
+# Question C histograms --------------------------------------------------
+
+
